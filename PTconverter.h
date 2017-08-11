2b235cc768850c3deac52967365bb079d8cdd1cc
Automatth <hrdfjmaris@gmail.com>
2017-08-11 19:02:19 -0300

    Add files via upload

---
+++ PTconverter.h
@@ -0,0 +1,72 @@
+/*---------------------------------------------------------------------------------
+  Eng. Helio R. de Freitas Jr.
+  junior700.eletro@gmail.com
+ ---------------------------------------------------------------------------------- 
+  this library intend to translate C language keywords to Portuguese
+  
+  Used new keywords:
+para = for
+principal = loop
+enquanto = while
+se = if
+entao = else
+para = for
+maior = >
+menor = <
+igual = ==
+inteiro = int
+caracter = char
+menu = switch
+caso = case
+vazio = void
+E = ||
+OU = &&
+e = |
+ou = &
+definicao = setup
+configuraPino = pinMode
+escrevePino = digitalWrite
+lePino	= digitalRead
+retardo = delay 	 
+  
+  You can use C mixing with PT new keywords in your sketch. 
+  Just include PTconverter.h in your sketch.
+  
+  
+  modifed 11 Aug 2017
+  by Eng. Helio R. Freitas Jr.
+  
+  
+-----------------------------------------------------------------------------------------  
+This software is released under an MIT license. See the attached LICENSE file for details.
+-----------------------------------------------------------------------------------------
+  
+  
+*/
+
+
+
+
+#define para for
+#define principal loop
+#define enquanto while
+#define se if
+#define entao else
+#define para for
+#define maior >
+#define menor <
+#define igual ==
+#define inteiro int
+#define caracter char
+#define menu switch
+#define caso case
+#define vazio void
+#define E ||
+#define OU &&
+#define e |
+#define ou &
+#define definicao setup
+#define configuraPino pinMode
+#define escrevePino digitalWrite
+#define lePino	digitalRead
+#define retardo delay 	 
